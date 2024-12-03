54ee047bee73248135fe1aa4c6602ecfe2a89278
ferfuentes <lzfer1@me.com>
2024-12-03 12:19:51 -0800

    Update Base.swift

--- Sources/NetworkLibrary/Base/Base.swift
+++ Sources/NetworkLibrary/Base/Base.swift
@@ -18,3 +18,37 @@ public protocol Base {
     var body: Data? { get }
     var boundry: String? { get }
 }
+
+
+extension Base {
+    public func createDataBody(withParameters params: [String: String]?, media: [Media]?) -> Data? {
+        let lineBreak = "\r\n"
+        var body = Data()
+        
+        guard let boundary = self.boundry else {
+            return nil
+        }
+
+        if let parameters = params {
+            for (key, value) in parameters {
+                body.append("--\(boundary + lineBreak)".data(using: .utf8)!)
+                body.append("Content-Disposition: form-data; name=\"\(key)\"\(lineBreak + lineBreak)".data(using: .utf8)!)
+                body.append("\(value + lineBreak)".data(using: .utf8)!)
+            }
+        }
+
+        if let media = media {
+            for photo in media {
+                body.append("--\(boundary + lineBreak)".data(using: .utf8)!)
+                body.append("Content-Disposition: form-data; name=\"\(photo.key)\"; filename=\"\(photo.fileName)\"\(lineBreak)".data(using: .utf8)!)
+                body.append("Content-Type: \(photo.mimeType + lineBreak + lineBreak)".data(using: .utf8)!)
+                body.append(photo.data)
+                body.append(lineBreak.data(using: .utf8)!)
+            }
+        }
+
+        body.append("--\(boundary)--\(lineBreak)".data(using: .utf8)!)
+
+        return body
+    }
+}
